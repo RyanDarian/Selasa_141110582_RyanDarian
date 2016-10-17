0518d938c33ad5d8380d2c8897bbe98d1015526b
RyanDarian <ryandarian1996@gmail.com>
2016-10-17 23:50:42 +0700

    Latihan 1_2 dan Latihan 2_2

---
+++ Selasa_141110582_RyanDarian/Latihan 1_2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Latihan_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.test = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(15, 22);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(309, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Min";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(439, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Max";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.min.Location = new System.Drawing.Point(309, 42);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(14, 20);
+            this.min.TabIndex = 3;
+            this.min.Text = "-";
+            this.min.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.max.Location = new System.Drawing.Point(439, 42);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(14, 20);
+            this.max.TabIndex = 4;
+            this.max.Text = "-";
+            this.max.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.max.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(310, 69);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(33, 128);
+            this.vScrollBar1.TabIndex = 5;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(443, 69);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(34, 127);
+            this.vScrollBar2.TabIndex = 6;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test.Location = new System.Drawing.Point(369, 216);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(14, 20);
+            this.test.TabIndex = 7;
+            this.test.Text = "-";
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 262);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.Label test;
+    }
+}
+
---
+++ Selasa_141110582_RyanDarian/Latihan 2_1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tanggal = new System.Windows.Forms.NumericUpDown();
+            this.Bulan = new System.Windows.Forms.DomainUpDown();
+            this.Tambah = new System.Windows.Forms.Button();
+            this.Hapus = new System.Windows.Forms.Button();
+            this.Kalender = new System.Windows.Forms.MonthCalendar();
+            this.Lahir = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Tanggal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tanggal
+            // 
+            this.Tanggal.Location = new System.Drawing.Point(14, 17);
+            this.Tanggal.Name = "Tanggal";
+            this.Tanggal.Size = new System.Drawing.Size(133, 20);
+            this.Tanggal.TabIndex = 0;
+            this.Tanggal.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Bulan
+            // 
+            this.Bulan.Location = new System.Drawing.Point(153, 17);
+            this.Bulan.Name = "Bulan";
+            this.Bulan.Size = new System.Drawing.Size(131, 20);
+            this.Bulan.TabIndex = 1;
+            this.Bulan.Text = "Bulan";
+            // 
+            // Tambah
+            // 
+            this.Tambah.Location = new System.Drawing.Point(290, 14);
+            this.Tambah.Name = "Tambah";
+            this.Tambah.Size = new System.Drawing.Size(85, 23);
+            this.Tambah.TabIndex = 2;
+            this.Tambah.Text = "Tambah";
+            this.Tambah.UseVisualStyleBackColor = true;
+            this.Tambah.Click += new System.EventHandler(this.Tambah_Click);
+            // 
+            // Hapus
+            // 
+            this.Hapus.Location = new System.Drawing.Point(381, 14);
+            this.Hapus.Name = "Hapus";
+            this.Hapus.Size = new System.Drawing.Size(83, 23);
+            this.Hapus.TabIndex = 3;
+            this.Hapus.Text = "Hapus";
+            this.Hapus.UseVisualStyleBackColor = true;
+            this.Hapus.Click += new System.EventHandler(this.Hapus_Click);
+            // 
+            // Kalender
+            // 
+            this.Kalender.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.Kalender.Location = new System.Drawing.Point(14, 49);
+            this.Kalender.Name = "Kalender";
+            this.Kalender.TabIndex = 4;
+            // 
+            // Lahir
+            // 
+            this.Lahir.AutoSize = true;
+            this.Lahir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lahir.Location = new System.Drawing.Point(12, 369);
+            this.Lahir.Name = "Lahir";
+            this.Lahir.Size = new System.Drawing.Size(161, 20);
+            this.Lahir.TabIndex = 5;
+            this.Lahir.Text = "Tanggal Lahir : 1 April";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 443);
+            this.Controls.Add(this.Lahir);
+            this.Controls.Add(this.Kalender);
+            this.Controls.Add(this.Hapus);
+            this.Controls.Add(this.Tambah);
+            this.Controls.Add(this.Bulan);
+            this.Controls.Add(this.Tanggal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Tanggal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown Tanggal;
+        private System.Windows.Forms.DomainUpDown Bulan;
+        private System.Windows.Forms.Button Tambah;
+        private System.Windows.Forms.Button Hapus;
+        private System.Windows.Forms.MonthCalendar Kalender;
+        private System.Windows.Forms.Label Lahir;
+    }
+}
+
